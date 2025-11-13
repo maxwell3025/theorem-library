1da203e8af2e3260ee184663ff928d8b46122e8d
maxwell3025 <amoinus@gmail.com>
2025-11-12 22:46:21 -0500

    feat: add timeout

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -3,11 +3,11 @@ import common.model as model
 import time
 
 
-def check_service_health(service_base: str):
+def check_service_health(service_base: str, timeout: float = 5):
     # backup in case httpx method fails -- this still allows us to get a timestamp
     backup_timer_start = time.perf_counter()
     try:
-        response = httpx.get(f"{service_base}/health")
+        response = httpx.get(f"{service_base}/health", timeout=timeout)
         response_time_ms = response.elapsed.microseconds // 1000
         status: model.DependencyHealthCheckStatus = (
             "healthy" if response.status_code == 200 else "unhealthy"
